57f6b8eb66281ac73040733fab9a7a3453800ea3
lht444 <lht444@hendrixgate03fl.unicph.domain>
2026-04-14 15:59:17 +0200

    slurm-script

---
+++ processed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ut extract --file_regex '/home/lht444/erda/sleep-data/resources/chat/polysomnography/edfs/*/*.edf' --out_dir '/home/lht444/erda/sleep-data/resources/processed/chat/' --resample 128 --channels F3-M2 F4-M1 C3-M2 C4-M1 T3-M2 T4-M1 O1-M2 O2-M1 E1-M2 E2-M1 --overwrite
+ut extract_hypno --file_regex '/home/lht444/erda/sleep-data/resources/chat/polysomnography/annotations-events-nsrr/*/*.xml' --out_dir '${local_path}/processed/chat/'
+ut cv_split --data_dir '/home/lht444/erda/sleep-data/resources/processed/chat/' --subject_dir_pattern 'chat*' --CV 1 --validation_fraction 0.10 --max_validation_subjects 50 --test_fraction 0.15 --max_test_subjects 100 --subject_matching_regex '.*?-.*?-(.*)'
---
+++ slurm-preprocessed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#The partition is the queue you want to run on. standard is gpu and can be ommitted.
+#SBATCH --job-name=U-Sleep-process
+#number of independent tasks we are going to start in this script
+#SBATCH --ntasks=1
+#number of cpus we want to allocate for each program
+#SBATCH --cpus-per-task=64
+#We expect that our program should not run longer than 2 days
+#Note that a program will be killed once it exceeds this time!
+#SBATCH --time=2-00:00:00
+#Skipping many options! see man sbatch
+# From here on, we can start our program
+
+./mount_erda.sh
+source /home/lht444/python-venv/usleep-keras/bin/activate
+ls
+./unmount_erda.sh
