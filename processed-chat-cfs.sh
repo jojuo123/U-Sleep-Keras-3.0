f9835027b6a8b93be12c830a8ebb58504d53310e
vtduy3100 <vtduy3100@gmail.com>
2026-04-14 19:04:15 +0200

    update

--- processed-chat-cfs.sh
+++ processed-chat-cfs.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-ut extract --log_dir logs-chat --overwrite --file_regex './erda2/sleep-data/resources/chat/polysomnography/edfs/*/*.edf' --out_dir '/home/lht444/erda2/sleep-data/resources/processed/chat/' --resample 128 --channels F3-M2 F4-M1 C3-M2 C4-M1 T3-M2 T4-M1 O1-M2 O2-M1 E1-M2 E2-M1
+ut extract --log_dir logs-chat --overwrite --file_regex './erda2/sleep-data/resources/chat/polysomnography/edfs/*/*.edf' --out_dir './erda2/sleep-data/resources/processed/chat/' --resample 128 --channels F3-M2 F4-M1 C3-M2 C4-M1 T3-M2 T4-M1 O1-M2 O2-M1 E1-M2 E2-M1
 
-ut extract_hypno --log_dir logs-chat --overwrite --file_regex './erda2/sleep-data/resources/chat/polysomnography/annotations-events-nsrr/*/*.xml' --out_dir '${local_path}/processed/chat/' 
+ut extract_hypno --log_dir logs-chat --overwrite --file_regex './erda2/sleep-data/resources/chat/polysomnography/annotations-events-nsrr/*/*.xml' --out_dir './erda2/sleep-data/resources/processed/chat/' 
 
 ut cv_split --data_dir './erda2/sleep-data/resources/processed/chat/' --subject_dir_pattern 'chat*' --CV 1 --validation_fraction 0.10 --max_validation_subjects 50 --test_fraction 0.15 --max_test_subjects 100 --subject_matching_regex '.*?-.*?-(.*)'
-
-ut extract --overwrite  --log_dir logs-cfs --file_regex './erda2/sleep-data/resources/cfs/polysomnography/edfs/*.edf' --out_dir './erda2/sleep-data/resources/processed/cfs/' --resample 128 --channels C3-A2 C4-A1 LOC-A2 ROC-A1 
-
-ut extract_hypno --overwrite --log_dir logs-cfs --file_regex './erda2/sleep-data/resources/cfs/polysomnography/annotations-events-nsrr/*.xml' --out_dir './erda2/sleep-data/resources/processed/cfs/'
-
-ut cv_split --data_dir './erda2/sleep-data/resources/processed/cfs/' --subject_dir_pattern 'cfs*' --CV 1 --validation_fraction 0.10 --max_validation_subjects 50 --test_fraction 0.15 --max_test_subjects 100 --subject_matching_regex '.*famID(.*)'
